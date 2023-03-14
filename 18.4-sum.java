8c823465c5c5ebab65ff97e51f0600fe535a3cff
CTWbottlemoon <mooninbottle@gmail.com>
2023-03-14 22:48:55 +0800

    Update 18.4-sum.java
    first solved:two pointer

--- 18.4-sum.java
+++ 18.4-sum.java
@@ -1,5 +1,6 @@
 import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.List;
 
 /*
  * @lc app=leetcode id=18 lang=java
@@ -10,10 +11,43 @@ import java.util.Arrays;
 // @lc code=start
 class Solution {
     public List<List<Integer>> fourSum(int[] nums, int target) {
+        List<List<Integer>> result = new ArrayList<>();
         Arrays.sort(nums);
-
-
+        long average = target / 4;
+        if(average < nums[0] || average > nums[nums.length-1]){
+            return result; 
+        }
+        List<Integer> tempResult =  new ArrayList<>();
         
+        for(int i = 0 ; i < nums.length  ; i++){
+            for(int j = i + 1 ; j< nums.length ; j++){
+                int tempTarget = target - nums[i] - nums[j];
+                int left = j + 1;
+                int right = nums.length - 1;
+                int sum = nums[left]+nums[right];
+                while(right > left){
+                    if(tempTarget == sum){
+                        tempResult.add(nums[i]);
+                        tempResult.add(nums[j]);
+                        tempResult.add(nums[left]);
+                        tempResult.add(nums[right]);
+                        if(!result.contains(tempResult)){
+                            result.add(tempResult);
+                        }
+                        tempResult.clear();
+                        left++;
+                        right--;
+                    }else if(tempTarget > sum){
+                        left++;
+                    }else{
+                        right--;
+                    }
+
+                }
+                
+            }
+        }
+        return result;
     }
 }
 // @lc code=end
